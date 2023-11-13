996b513caa41c1e3097b70dd842e70e1c20adbe5
elena <ellena_purice@yahoo.com>
2023-11-13 15:31:01 +0100

    navbar modifiz

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -43,7 +43,7 @@ const Navbar = () => {
           duration={500}
           className="desktopMenuListItem"
         >
-          Portfolio
+          Projects
         </Link>
         <Link
           activeClass="active"
